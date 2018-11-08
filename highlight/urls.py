cccdcfbc7926860aa32d92172df450ba11d3fa74
Heunnnn <0987haho>
2018-11-08 19:41:01 +0900

    뷰 생성/request 설정 완료

--- highlight/urls.py
+++ highlight/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path('extract/', views.extract, name='extract'),
+    path('result/', views.result, name='result'),
+    path('example/', views.example, name='example'),
 ]
